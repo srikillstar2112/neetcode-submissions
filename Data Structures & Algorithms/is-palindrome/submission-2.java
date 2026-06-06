51e6d4cb48b553e6734526336555167746228a77
srikillstar2112 <156598968+srikillstar2112@users.noreply.github.com>
2026-06-06 15:34:11 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String rep=s.toLowerCase().replaceAll("[^a-zA-Z0-9]","");
+        int left=0;
+        int right=rep.length()-1;
+        int count=0;
+        while(left<right){
+            if(rep.charAt(left)!=rep.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+    return true;
+        }
+}
