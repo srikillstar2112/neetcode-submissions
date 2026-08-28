584f72767d042ab2433988d4af3342ffcb8c4a28
srikillstar2112 <156598968+srikillstar2112@users.noreply.github.com>
2026-08-28 09:42:34 +0530

    Add: target-sum - submission-1

---
+++ Data Structures & Algorithms/target-sum/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int tot=0;
+        for(int n:nums){
+            tot+=n;
+        }
+        int off=tot;
+        int[][] dp=new int[nums.length+1][2*tot+1];
+        dp[0][off]=1;
+        for(int i=0;i<nums.length;i++){
+            for(int sum=-tot;sum<=tot;sum++){
+                int idx=sum+off;
+                if(dp[i][idx]==0){
+                    continue;
+                }
+                dp[i+1][sum+nums[i]+off]+=dp[i][idx];
+                dp[i+1][sum-nums[i]+off]+=dp[i][idx];
+            }
+        }
+        return dp[nums.length][target+off];
+    }
+}
